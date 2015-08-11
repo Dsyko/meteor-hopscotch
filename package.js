5f274378fd5970c75ac199509d766d6cd5cc2284
Mike king <mikkelking@hotmail.com>
2015-08-11 22:46:30 +1000

    New

--- package.js
+++ package.js
@@ -1,14 +1,19 @@
 Package.describe({
-	summary:  "Hopscotch is a framework that makes it easy for developers to add product tours to their pages.",
-	version: '1.1.4',
-	name: "mikkelking:hopscotch",
+	summary:  "Hopscotch (v0.25) - tour plugin for Meteor",
+	version: '1.0.0',
+	name: "mikkelking:meteor-hopscotch",
 	git: 'https://github.com/mikkelking/meteor-hopscotch'
 });
 
 Package.onUse(function (api) {
-	api.versionsFrom("METEOR@1.1.0.2");
+	api.versionsFrom("METEOR@1.1.0.3");
     api.addFiles('lib/hopscotch-0.2.5.min.js', "client");
     api.addFiles('lib/hopscotch-0.2.5.min.css', "client");
-    api.addFiles('img/sprite-green-0.3.png', "client");
-    api.addFiles('img/sprite-orange-0.3.png', "client");
+    api.addFiles('lib/main.js', "client");
+	api.export('hopscotch', 'client');
+});
+
+Package.onTest(function (api) {
+	api.use(['mikkelking:meteor-hopscotch', 'tinytest'], ['client']);
+	api.add_files('test.js', ['client']);
 });
